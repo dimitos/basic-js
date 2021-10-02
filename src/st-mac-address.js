aae4a91be5045ca3e938dca9b40647f8f132b3c7
dimitos <kurort26@mail.ru>
2021-10-02 23:17:13 +0300

    feat: add getMatrixElementsSum functions

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  n = n.split('-');
+  if (n.length !== 6) return false;
+  for (let i = 0; i < n.length; i++) {
+    if (!n[i][0].match(/[A-F0-9]/g) || !n[i][1].match(/[A-F0-9]/g)) return false;
+  }
+  return true;
 }
