07b7caa2607339ff4088d49812feb4fb8318e1d6
dimitos <kurort26@mail.ru>
2021-10-02 16:39:41 +0300

    feat: add sortByHeight functions

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,8 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let cnt = 0,
+    r = arr.filter((el) => el !== -1).sort((a, b) => a - b);
+  return arr.map((el) => (el !== -1 ? r[cnt++] : -1));
 }
